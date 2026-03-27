a25d5abcac6dd06e2cfaf3b03319471a7a135b16
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-27 16:03:17 +0200

    feat: add main application logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,144 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from "./components/layouts/Layout";
+import Welcome from "./components/layouts/Welcome";
+import Dashboard from "./components/layouts/Dashboard";
+import Challenge from "./components/layouts/Challenge";
+import { useState, useEffect } from "react";
+
+import WORDS from "./utils/VOCAB.json";
+
+import { countdownIn24Hours, getWordByIndex, PLAN } from "./utils";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [name, setName] = useState("");
+  const [day, setDay] = useState(1);
+  const [datetime, setDatetime] = useState(null);
+  const [history, setHistory] = useState({});
+  const [attempts, setAttempts] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const daysWords = PLAN[day].map((idx) => {
+    return getWordByIndex(WORDS, idx).word;
+  });
+
+  function handleChangePage(pageIndex) {
+    setSelectedPage(pageIndex);
+  }
+
+  function handleCreateAccount() {
+    if (!name) {
+      return;
+    }
+    localStorage.setItem("username", name);
+    handleChangePage(1);
+  }
+
+  function handleCompleteDay() {
+    const newDay = day + 1;
+    const newDatetime = Date.now();
+    setDay(newDay);
+    setDatetime(newDatetime);
+
+    localStorage.setItem(
+      "day",
+      JSON.stringify({
+        day: newDay,
+        datetime: newDatetime,
+      }),
+    );
+    setSelectedPage(1);
+  }
+
+  function handleIncrementAttempts() {
+    const newRecord = attempts + 1;
+    localStorage.setItem("attempts", newRecord);
+    setAttempts(newRecord);
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    if (localStorage.getItem("username")) {
+      setName(localStorage.getItem("username"));
+
+      setSelectedPage(1);
+    }
+
+    if (localStorage.getItem("attempts")) {
+      setAttempts(parseInt(localStorage.getItem("attempts")));
+    }
+
+    if (localStorage.getItem("history")) {
+      setHistory(JSON.parse(localStorage.getItem("history")));
+    }
+
+    if (localStorage.getItem("day")) {
+      const { day: d, datetime: dt } = JSON.parse(localStorage.getItem("day"));
+      setDatetime(dt);
+      setDay(d);
+
+      if (d > 1 && dt) {
+        const diff = countdownIn24Hours(dt);
+        if (diff < 0) {
+          let newHistory = { ...history };
+          const timestamp = new Date(dt);
+          const formattedTimestamp = timestamp
+            .toString()
+            .split(" ")
+            .slice(1, 4)
+            .join(" ");
+          newHistory[formattedTimestamp] = d;
+          setHistory(newHistory);
+          setDay(1);
+          setDatetime(null);
+          setAttempts(0);
+
+          localStorage.setItem("attempts", 0);
+          localStorage.setItem("history", JSON.stringify(newHistory));
+          localStorage.setItem(
+            "day",
+            JSON.stringify({ day: 1, datetime: null }),
+          );
+        }
+      }
+    }
+  }, []);
+
+  const pages = {
+    0: (
+      <Welcome
+        handleCreateAccount={handleCreateAccount}
+        username="hello world"
+        name={name}
+        setName={setName}
+      />
+    ),
+    1: (
+      <Dashboard
+        history={history}
+        name={name}
+        attempts={attempts}
+        PLAN={PLAN}
+        day={day}
+        handleChangePage={handleChangePage}
+        daysWords={daysWords}
+        datetime={datetime}
+      />
+    ),
+    2: (
+      <Challenge
+        day={day}
+        daysWords={daysWords}
+        handleChangePage={handleChangePage}
+        handleIncrementAttempts={handleIncrementAttempts}
+        handleCompleteDay={handleCompleteDay}
+        PLAN={PLAN}
+      />
+    ),
+  };
+
+  return <Layout>{pages[selectedPage]}</Layout>;
 }
 
-export default App
+export default App;
